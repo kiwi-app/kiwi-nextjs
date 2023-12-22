0dc3133317aa0dc09f464bd5ad04de2ad34bcfcb
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2023-12-22 10:07:52 -0300

    fetch page at server side

--- src/components/CatchAll.tsx
+++ src/components/CatchAll.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { cookies, headers } from 'next/headers';
-import { mergeManifestSections } from '../helpers';
+import { getLoaderProps, getPageConfig, mergeManifestSections } from '../helpers';
 import CatchAllLive from './CatchAllLive';
 import CatchAllServer from './CatchAllServer';
 import { LoaderRequest } from '../types';
@@ -8,14 +8,12 @@ import { LoaderRequest } from '../types';
 export type CatchAllProps = { params: { kiwi: string[] } };
 
 export default function KiwiCatchAll(manifest: any, live: boolean = false) {
-  return function CatchAll({ params: { kiwi } }: CatchAllProps) {
+  return async function CatchAll({ params: { kiwi } }: CatchAllProps) {
     const mergedManifest = mergeManifestSections(manifest);
     let requestInfo: LoaderRequest = {
       cookies: {},
       headers: {},
     };
-
-    console.log('kiwi ->', kiwi);
 
     if (!live) {
       cookies()
@@ -29,9 +27,24 @@ export default function KiwiCatchAll(manifest: any, live: boolean = false) {
       });
     }
 
+    const page = await getPageConfig(manifest.site, kiwi.join('/'));
+    if (!page) return null;
+
+    for (let idx = 0; idx < page.sections?.length; idx++) {
+      const section = page.sections[idx];
+      const sectionModule = manifest.sections[section.type];
+
+      page.sections[idx].props = await getLoaderProps(
+        requestInfo,
+        section.props,
+        sectionModule,
+        manifest,
+      );
+    }
+
+    console.log('page', page);
+
     if (live) return <CatchAllLive requestInfo={requestInfo} manifest={mergedManifest} />;
-    return (
-      <CatchAllServer requestInfo={requestInfo} manifest={mergedManifest} kiwi={kiwi.join('/')} />
-    );
+    return <CatchAllServer page={page} manifest={mergedManifest} />;
   };
 }
