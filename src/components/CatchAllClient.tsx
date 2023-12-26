5367db7a4fa9a146b445ef7748065b3d5b2d7a0f
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2023-12-26 13:29:05 -0300

    bugfixxxxxx

--- src/components/CatchAllClient.tsx
+++ src/components/CatchAllClient.tsx
@@ -66,7 +66,7 @@ export default (externalManifest: any) =>
         newPage.sections[idx].props = await getLoaderProps(
           requestInfo,
           section.props,
-          page.sections[idx].props,
+          page.sections[idx]?.props || {},
           sectionModule,
           manifest,
         );
