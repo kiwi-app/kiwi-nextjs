2fbd80e1a22804cdadb8ee5320d6b440a5c93aeb
Luann Francisco <dudow8@gmail.com>
2024-01-08 02:01:34 -0800

    feat: add generate to the cli command list

--- src/cli/index.js
+++ src/cli/index.js
@@ -2,12 +2,14 @@
 
 const init = require('./init');
 const manifest = require('./manifest');
+const generate = require('./generate');
 
 const [action, ...args] = process.argv.slice(2);
 
 const actions = {
   'init': init,
   'manifest': manifest,
+  'generate': generate,
 }
 
 async function executeCli() {
